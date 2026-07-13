02f85b335d934d465c7ac4fabda1bff22065254c
Julian <git@julian-haas.de>
2026-07-13 09:54:49 +0200

    vibecoded PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,54 @@
 import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [scalaJSPlugin()],
+  plugins: [
+    scalaJSPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "pwa-icon.svg",
+        "apple-touch-icon-180x180.png",
+      ],
+      manifest: {
+        name: "Fahrtenbuch",
+        short_name: "Fahrtenbuch",
+        description: "Digitales Fahrtenbuch",
+        theme_color: "#3273dc",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        lang: "de",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}"],
+        navigateFallback: "index.html",
+      },
+    }),
+  ],
 });
